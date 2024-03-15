ed9e83f1963f42db6035da7d38ca30770b7c3dfa
Аникьев Евгений Юрьевич <anikjevjenya@gmail.com>
2024-03-15 16:44:35 +0300

    1

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,51 @@
 #include <iostream>
 #include <SDL.h>
 
+SDL_Window* win = NULL;
+SDL_Renderer* render = NULL;
+
+void DeInit(int error)
+{
+	if (render != NULL) SDL_DestroyRenderer(render);
+	if (win != NULL) SDL_DestroyWindow(win);
+	SDL_Quit();
+	exit(error);
+}
+
+void Init()
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		printf("Init Error! - %s", SDL_GetError());
+		system("pause");
+		DeInit(1);
+	}
+
+	SDL_Window* win = SDL_CreateWindow("Just window", 20, 50, 800, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+
+	if (win == NULL)
+	{
+		printf("Window Error! - %s", SDL_GetError());
+		system("pause");
+		DeInit(1);
+	}
+
+	SDL_Renderer* render = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+
+	if (render == NULL)
+	{
+		printf("Renderer Error! - %s", SDL_GetError());
+		system("pause");
+		DeInit(1);
+	}
+}
+
 int main(int arcg, char* argv[])
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
+	Init();
 
+	SDL_Delay(5000);
 
-
-	SDL_Quit();
+	DeInit(0);
 	return 0;
 }
