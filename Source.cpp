4f9289d3bd4cee4fa79027f6f4ea180c0ff98de0
Аникьев Евгений Юрьевич <anikjevjenya@gmail.com>
2024-03-27 22:12:27 +0300

    1

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,7 @@ SDL_Window* win = NULL;
 SDL_Renderer* render = NULL;
 
 int win_width = 800, win_height = 600;
+int Frames[3] = { 0, 0, 0 };
 
 int x = 0;
 int y = 0;
@@ -17,6 +18,14 @@ bool isDownPressed = false;
 struct HouseWindow
 {
 	int x, y;
+	SDL_Color color;
+};
+
+struct Firework
+{
+	float x, y;
+	float vx, vy;
+	int frames_to_reapear;
 	SDL_Color color;
 };
 
@@ -86,13 +95,22 @@ int random(int a, int b)
 	return rand()%(b-a+1)+a;
 }
 
-void DrawWindows(HouseWindow window, int upx, int upy)
+void DrawSmallWindows(HouseWindow window, int upx, int upy)
 {
 	SDL_SetRenderDrawBlendMode(render, SDL_BLENDMODE_BLEND);
 
 		SDL_SetRenderDrawColor(render, window.color.r, window.color.g, window.color.b, window.color.a);
 		SDL_Rect win_points = { window.x+upx, window.y + upy, 40, 30 };
 		SDL_RenderFillRect(render, &win_points);
+}
+
+void DrawBigWindows(HouseWindow window, int upx, int upy)
+{
+	SDL_SetRenderDrawBlendMode(render, SDL_BLENDMODE_BLEND);
+
+	SDL_SetRenderDrawColor(render, window.color.r, window.color.g, window.color.b, window.color.a);
+	SDL_Rect win_points = { window.x + upx, window.y + upy, 30, 40 };
+	SDL_RenderFillRect(render, &win_points);
 }
 
 void UpdateWindow(bool isLightUp, HouseWindow smallwindow[])
@@ -114,6 +132,77 @@ void UpdateWindow(bool isLightUp, HouseWindow smallwindow[])
 
 	}
 }
+
+
+void DrawFirework(Firework firework[3][4])
+{
+	SDL_SetRenderDrawBlendMode(render, SDL_BLENDMODE_BLEND);
+	for (int i = 0; i < 3; i++)
+	{
+		float firerectside = 15;
+		for (int j = 0; j < 4; j++)
+		{ 
+			SDL_SetRenderDrawColor(render, firework[i][j].color.r , firework[i][j].color.g, firework[i][j].color.b, firework[i][j].color.a);
+			SDL_Rect Firework = {firework[i][j].x, firework[i][j].y, firerectside, firerectside };
+			SDL_RenderFillRect(render, &Firework);
+		}
+	}
+}
+
+void UpdateFirework(Firework firework[3][4], int c)
+{
+	if (Frames[c] > 0)
+	{
+		Frames[c]-=1;
+		return;
+	}
+		float onex = (float)random(0, 800);
+		float oney = (float)random(0, 350);
+		float onevx = (float)random(2, 6);
+		float onevy = (float)random(2, 9);
+		
+		unsigned char r = random(0, 255);
+		unsigned char g = random(0, 255);
+		unsigned char b = random(0, 255);
+		
+		for (int j = 0; j < 4; j++)
+		{
+			if (j == 0)
+			{
+				firework[c][j].x += firework[c][j].vx;
+				firework[c][j].y += firework[c][j].vy;
+				firework[c][j].vy += 0.2;
+			}
+			if (j == 1)
+			{
+				firework[c][j].x -= firework[c][j].vx;
+				firework[c][j].y += firework[c][j].vy;
+				firework[c][j].vy += 0.2;
+			}
+			if (j == 2)
+			{
+				firework[c][j].x += firework[c][j].vx;
+				firework[c][j].y += firework[c][j].vy;
+				firework[c][j].vy += 0.2;
+			}
+			if (j == 3)
+			{
+				firework[c][j].x -= firework[c][j].vx;
+				firework[c][j].y += firework[c][j].vy;
+				firework[c][j].vy += 0.2;
+			}
+		}
+		if (((firework[c][0].y > win_height + 30) || (firework[c][0].x > win_width + 30)) && ((firework[c][1].y > win_height + 30) || (firework[c][1].x > win_width + 30)) && ((firework[c][2].y > win_height + 30) || (firework[c][2].x > win_width + 30)) && ((firework[c][3].y > win_height + 30) || (firework[c][3].x > win_width + 30)))
+		{
+			Frames[c] = random(0,10);
+			firework[c][0] = {onex, oney, onevx, -onevy, random(0,200), r, g, b, 255};
+			firework[c][1] = { onex, oney, onevx, -onevy, random(0,200), r, g, b, 255};
+			firework[c][2] = { onex, oney, onevx, onevy, random(0,200), r, g, b, 255};
+			firework[c][3] = { onex, oney, onevx, onevy, random(0,200), r, g, b, 255};
+		}
+	
+}
+
 int main(int arcg, char* argv[])
 {
 	srand(time(NULL));
@@ -122,12 +211,38 @@ int main(int arcg, char* argv[])
 	
 	bool isRunning = true;
 	
+	HouseWindow bigwindow[16];
 	HouseWindow smallwindow[16];
+	Firework firework[3][4];
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			firework[i][j].frames_to_reapear = 0;
+			firework[i][j].x = win_width + 100;
+			firework[i][j].y = win_height + 100;
+			firework[i][j].vx = 10;
+			firework[i][j].vy = 0;
+		}
+
+	}
+	
+
+	for (int i = 0; i < 16; i++)
+	{
+		bigwindow[i].x = 275;
+		bigwindow[i].y = 480;
+		bigwindow[i].color.r = 255;
+		bigwindow[i].color.g = 240;
+		bigwindow[i].color.b = 0;
+		bigwindow[i].color.a = random(0, 255);
+	}
 
 	for (int i = 0; i < 16; i++)
 	{
 		smallwindow[i].x = 10;
-		smallwindow[i].y = 510;
+		smallwindow[i].y = 520;
 		smallwindow[i].color.r = 255;
 		smallwindow[i].color.g = 240;
 		smallwindow[i].color.b = 0;
@@ -138,6 +253,12 @@ int main(int arcg, char* argv[])
 	while (isRunning)
 	{
 		
+		UpdateWindow(isLightUp, smallwindow);
+		UpdateWindow(isLightUp, bigwindow);
+		for (int i = 0; i < 3; i++)
+		{
+			UpdateFirework(firework, i);
+		}
 
 		SDL_SetRenderDrawColor(render, 10, 5, 65, 255);
 		SDL_RenderClear(render);
@@ -150,24 +271,46 @@ int main(int arcg, char* argv[])
 		{
 			
 			if (upx > 180 && upx <250 ) upx += 215;
-			DrawWindows(smallwindow[i],upx,upy);
+			DrawSmallWindows(smallwindow[i],upx,upy);
 			upx += 60;
 		}
 		upx = 0;
 		upy = 0;
 		for (int i = 8; i < 16; i++)
 		{
-			upy = 55;
+			upy = 50;
 			if (upx > 180 && upx < 250) upx += 215;
-			DrawWindows(smallwindow[i], upx, upy);
+			DrawSmallWindows(smallwindow[i], upx, upy);
 			upx += 60;
 			
 		}
-		UpdateWindow(isLightUp, smallwindow);
-		SDL_RenderPresent(render);
 		
+		upx = 0;
+		upy = 0;
+		for (int i = 0; i < 8; i++)
+		{
 
-		SDL_Delay(20);
+			if (upx > 160 && upx < 250) upx += 275;
+			DrawBigWindows(bigwindow[i], upx, upy);
+			upx += 60;
+		}
+		upx = 0;
+		upy = 0;
+		for (int i = 8; i < 16; i++)
+		{
+			upy = 70;
+			if (upx > 160 && upx < 250) upx += 275;
+			DrawBigWindows(bigwindow[i], upx, upy);
+			upx += 60;
+
+		}
+		
+		DrawFirework(firework);
+
+		
+		SDL_RenderPresent(render);
+
+		SDL_Delay(50);
 	}
 
 	DeInit(0);
